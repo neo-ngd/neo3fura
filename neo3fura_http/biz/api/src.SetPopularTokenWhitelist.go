d101eeebae67442c9d12c7a8a1d2056e3d282e7e
Tobeyw <tobey1024@126.com>
2026-03-31 12:32:17 +0800

    delete total count  for cursor

--- neo3fura_http/biz/api/src.SetPopularTokenWhitelist.go
+++ neo3fura_http/biz/api/src.SetPopularTokenWhitelist.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"encoding/json"
-	"go.mongodb.org/mongo-driver/bson"
 	"neo3fura_http/lib/type/h160"
 	"neo3fura_http/var/stderr"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (me *T) SetPopularTokenWhitelist(args struct {
@@ -19,26 +21,27 @@ func (me *T) SetPopularTokenWhitelist(args struct {
 		hashArr = append(hashArr, item)
 	}
 	r1, _, err := me.Client.QueryAllWithCursor(struct {
-		Collection string
-		Index      string
-		Sort       bson.M
-		Filter     bson.M
-		Query      []string
-		Limit      int64
-		Skip       int64
+		Collection   string
+		Index        string
+		Sort         bson.M
+		Filter       bson.M
+		Query        []string
+		Limit        int64
+		Skip         int64
 		CursorFilter bson.M
 	}{
-		Collection: "Asset",
-		Index:      "SetPopularTokens",
-		Sort:       bson.M{},
-		Filter:     bson.M{"hash": bson.M{"$in": hashArr}},
-		Query:      []string{},
+		Collection:   "Asset",
+		Index:        "SetPopularTokens",
+		Sort:         bson.M{},
+		Filter:       bson.M{"hash": bson.M{"$in": hashArr}},
+		Query:        []string{},
 		CursorFilter: nil,
 	}, ret)
 	if err != nil {
 		return err
 	}
 
+	var values []string
 	var data []interface{}
 	for _, item := range r1 {
 		asset := bson.M{
@@ -50,6 +53,7 @@ func (me *T) SetPopularTokenWhitelist(args struct {
 			"type":        item["type"],
 		}
 		data = append(data, asset)
+		values = append(values, item["hash"].(string))
 	}
 	success, err := me.Client.SaveManyJob(struct {
 		Collection string
@@ -58,6 +62,17 @@ func (me *T) SetPopularTokenWhitelist(args struct {
 	if err != nil {
 		return err
 	}
+
+	timestamp := time.Now().UnixMilli()
+	data1 := bson.M{"Populars": values, "insertTime": timestamp}
+	_, err = me.Client.SaveJob(struct {
+		Collection string
+		Data       bson.M
+	}{Collection: "PopularTokens", Data: data1})
+	if err != nil {
+		return err
+	}
+
 	result := make(map[string]interface{})
 	if success {
 		result["msg"] = "Insert document done!"
